5c2b150a81d5e7b588148d02dc75aa1eb245f409
carlos8788 <carlos8788@gmail.com>
2023-06-19 19:22:22 -0300

    ejercicios_4_5_6

--- src/ComponenteB.jsx
+++
@@ -1,9 +0,0 @@
-export default class Contacto {
-
-    constructor(nombre, apellido, email, conectado){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.conectado = conectado;
-    }
-}
